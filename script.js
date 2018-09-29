8bad297ae52490e6ce37f7f94ba18cb8e144a944
goKeitRu <43572733+goKeitRu@users.noreply.github.com>
2018-09-29 23:00:51 +0300

    изучение введения

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert('lets js');
+obj = {
+    name: "John",
+    age: 24,
+    isMarried: false,
+ }
+console.log(obj["name"]);
+
+var arr = ['apple', 'orange', 'plum', 'la', 'cri', 'lo', 'jag']
+console.log(arr[5]);
+
+// confirm("Тут?");
+var answer = prompt("что скажешь?", "42");
+console.log( typeof(arr));
