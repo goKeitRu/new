c3a14ef1536b22d8ce24e464ef73b0689c1dfbc9
goKeitRu <43572733+goKeitRu@users.noreply.github.com>
2018-09-30 20:44:50 +0300

    Нужный кусок кода

--- script.js
+++ script.js
@@ -11,3 +11,33 @@
 // // confirm("Тут?");
 // // var answer = prompt("что скажешь?", "42");
 // console.log( typeof(arr));
+let num = 50;
+
+if (num < 49) {
+	console.log('неверно')
+} else if (num > 100) {
+	console.log('неверно')
+} else {
+	console.log('да неужели!')
+}
+
+(num == 50) ? console.log('да неужели!'): console.log('неверно')
+
+switch (num) {
+	case 49:
+		console.log('мало');
+		break;
+	case 100:
+		console.log('много');
+		break;
+	case 80:
+		console.log('теплее');
+		break;
+	case 50:
+		console.log('верно!');
+		break;
+	default:
+		console.log('не в этот раз');
+		break;
+
+}
